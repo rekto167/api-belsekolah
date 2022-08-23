93a8e9cb2a971264a4f755ea326c2a5d95d22f10
Renal Eki Riyanto <renalekiriyanto@gmail.com>
2022-08-23 17:19:48 +0700

    belsekolah

--- app/Http/Controllers/API/BelController.php
+++ app/Http/Controllers/API/BelController.php
@@ -35,7 +35,9 @@ class BelController extends Controller
                 'name'=>$request->name
             ]);
 
-            return response()->json($day);
+            return ResponseFormatter::success([
+                'data'=>$day
+            ],'Add day successfully!');
         } catch (Exception $error) {
             return response()->json($error,500);
         }
@@ -45,21 +47,62 @@ class BelController extends Controller
         try {
             $id->delete();
 
-            // return Response::json([
-            //     'message'=>$id->name.'was deleted'
-            // ],200);
+            return ResponseFormatter::success([
+                'data'=>$id
+            ],'This day was deleted!');
         } catch (Exception $error) {
             return response()->json($error,500);
         }
     }
     // DAY
 
+    // ACTIVITY
+    // GET
     public function getActivities(){
         try {
             $activities = Activity::all();
-            return response()->json($activities);
+            return ResponseFormatter::success([
+                'data'=>$activities
+            ],'Fetch activities successfully!');
         } catch (Exception $error) {
             return response()->json($error,500);
         }
     }
+    // CREATE
+    public function createActivity(Request $request){
+        try {
+            $request->validate([
+                'name'=>'required'
+            ]);
+
+            $activity = Activity::create([
+                'name'=>$request->name
+            ]);
+
+            return ResponseFormatter::success([
+                'data'=>$activity
+            ],'Add activity was successfully!');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'error'=>$error,
+            ],'Something wrong bro!');
+        }
+    }
+    // DELETE
+    public function deleteActivity($id){
+        try {
+            $activity = Activity::find($id);
+
+            $activity->delete();
+
+            return ResponseFormatter::success([
+                'data'=>$activity
+            ],'Activity delete successfully!');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'error'=>$error,
+            ],'Something wrong bro!');
+        }
+    }
+    // ACTIVITY
 }
